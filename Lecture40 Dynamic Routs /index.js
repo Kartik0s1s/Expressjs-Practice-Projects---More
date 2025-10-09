55ca43b74b668f0dae96e96c0309101cc1efa348
Kartik Chaudhar <chaudharikartik532@gmail.com>
2025-10-09 08:45:19 +0530

    i have Implemented Dynamic Routes

---
+++ Lecture40 Dynamic Routs /index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { link } from 'fs';
+const app = express();
+
+app.get("/", (req, resp) => {
+const users=['ram', 'shyam', 'raju'];
+let data=`<ul>`;
+for ( let i =0 ; i<users.length;i++){
+  data+=`<li> <a href="user/${users[i]}"> ${users[i]}</a> </li>`
+   
+}
+
+data+='</ul>'
+ resp.send(data) 
+});
+
+app.get("/user/:name", (req, resp)=>{
+    const userName= req.params.name;
+    resp.send(`This is ${userName}'s Profile`)
+})
+
+
+        
+ 
+      
+ 
+      
+app.listen(3000, () => {
+  console.log("✅ Server running on http://localhost:3000");
+});
